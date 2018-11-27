d9ec8172c371287f9755202ca65a014c15d38485
Kevin Choy <freecancerian@gmail.com>
2018-11-27 12:28:21 +0000

    add pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :destroy, :show]
   
   def index
-    @articles = Article.all
-    
+    @articles = Article.paginate(page: params[:page], per_page: 3)
   end 
   
   def new
