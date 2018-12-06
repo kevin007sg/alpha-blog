9a1ac6f9c1c7202351ec2cf1d2ffdd927c4ee0d5
Kevin Choy <freecancerian@gmail.com>
2018-12-06 11:05:40 +0000

    complete authenttication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
 
   before_action :set_article, only: [:edit, :update, :destroy, :show]
+  before_action :required_user, except: [:index, :show]
+  before_action :required_same_user, only: [:edit, :update, :destroy]
   
   def index
     @articles = Article.paginate(page: params[:page], per_page: 3)
@@ -55,5 +57,12 @@ class ArticlesController < ApplicationController
     
     def set_article
       @article = Article.find(params[:id])
+    end
+    
+    def required_same_user
+      if current_user != @article.user
+        flash[:danger] = "You can only edit / delete your own article."
+        redirect_to root_path
+      end
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action :required_same_user, only: [:edit, :update]
+  
+  
   def index
     @users = User.paginate(page: params[:page], per_page: 3)
   end
@@ -19,11 +23,9 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "You account was updated successfully."
       redirect_to articles_path
@@ -33,12 +35,21 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])  
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
   
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def required_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
+    end
   end
 end
